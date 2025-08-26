c497934014158cf8a9d54a3d3aac200218ee4646
ehab elhelw <popehab057@gmail.com>
2025-08-26 06:22:58 +0300

    Add project files.

---
+++ EFCoreSession1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreSession1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
